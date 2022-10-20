2adba8d3307baad1f1e5b710f1da1118322ce4ea
RajNalla123 <rajashekarreddy606@gmail.com>
2022-10-20 10:55:45 +0530

    pushing one project

---
+++ practice/src/test/java/gitbashpractice/Gitbash.java
@@ -0,0 +1,11 @@
+package gitbashpractice;
+
+public class Gitbash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my git bash");
+
+	}
+
+}
